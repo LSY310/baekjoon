d00121dffc4e4ad08974dbe12adac51b8a3a4258
LIM0310 <limsy0310@gmail.com>
2024-05-09 16:21:56 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -1,7 +1,7 @@
-n = int(input())
-
-for _ in range(n):
-	money = int(input())
-	for i in [25, 10, 5, 1]:
-		print(money//i, end=' ')
-		money = money%i
+n=int(input())
+m=[25,10,5,1]
+for j in range(n):
+    n2=int(input())
+    for i in m:
+        print(n2//i, end=' ')
+        n2=n2%i
